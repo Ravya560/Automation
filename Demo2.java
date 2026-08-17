c524240fee2889bf2978b018c20e0f386254080c
Nupur Tuteja <nupurtuteja@yahoo.co.in>
2026-08-17 17:21:58 +0530

    Added error

--- Demo2.java
+++ Demo2.java
@@ -3,11 +3,13 @@ import java.sql.*;
 import java.util.Scanner;
 
 public class Demo2 {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+    public staticc void main(String[] args) throws ClassNotFoundException, SQLException {
         Class.forName("com.mysql.cj.jdbc.Driver");
         String url = "jdbc:mysql://localhost:3306/universalit";
         String uname = "root";
         String password = "Ravya@123";
+        /*sout ---> Nupur*/
         Connection con = DriverManager.getConnection(url, uname, password);
         Statement st = con.createStatement();
         ResultSet rs = st.executeQuery("select * from accounts");
